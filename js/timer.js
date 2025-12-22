a8ab50c2717f473b11898ad9bf8923751bb75c6e
Aneni Kidanu Manale <anenikidanu@gmail.com>
2025-12-23 01:34:31 +0300

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,6 @@
 let timer = 600; 
- const timeBox = document.getElementById("timer");const timerInterval = setInterval(() => {
+ const timeBox = document.getElementById("timer");
+const timerInterval = setInterval(() => {
     if (timer <= 0) {
       clearInterval(timerInterval);
       gradeQuiz();
@@ -10,4 +11,4 @@ let timer = 600;
     const min = Math.floor(timer / 60);
     const sec = timer % 60;
     timeBox.textContent = Time: ${min}:${sec.toString().padStart(2, "0")};
-  }, 1000); add tghis on js
+  }, 1000);
